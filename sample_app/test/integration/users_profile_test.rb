df86c4018cf63a7d952265df77d6792419d6de1c
well147 <wellerson_drumond@hotmail.com>
2020-04-23 18:12:08 -0300

    Add follow methods and following and followers pages

--- sample_app/test/integration/users_profile_test.rb
+++ sample_app/test/integration/users_profile_test.rb
@@ -16,5 +16,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+
+    # Tests if the number os followers and following appers in the layout
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
   end
 end
