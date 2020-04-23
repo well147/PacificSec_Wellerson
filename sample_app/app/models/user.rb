c1ebefc27c708bf587d7c936c1a558dfe3ecec2d
well147 <wellerson_drumond@hotmail.com>
2020-04-23 20:34:40 -0300

    Add follow methods and following and followers pages

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -4,6 +4,18 @@ class User < ApplicationRecord
   class_name:  'Relationship',
   foreign_key: 'follower_id',
   dependent:   :destroy
+  
+  has_many :passive_relationships, 
+  class_name:  'Relationship',
+  foreign_key: 'followed_id',
+  dependent:   :destroy
+
+  has_many :following,
+  through: :active_relationships,
+  source:  :followed
+  
+  has_many :followers,
+  through: :passive_relationships
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
@@ -91,6 +103,21 @@ class User < ApplicationRecord
   # Define a proto-feed
   def feed
     Micropost.where('user_id = ?', id)
+  end
+
+  # Follow a user
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns tru if the current user is following the other user
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
